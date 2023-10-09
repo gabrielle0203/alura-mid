4e427e17f795fe0c47aee26c15bd20988d7c9a4b
gabrielle0203 <129885383+gabrielle0203@users.noreply.github.com>
2023-10-09 14:02:10 -0300

    Add files via upload

--- mein.js
+++ mein.js
@@ -1,6 +1,22 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(idElementoAudio){
+document.querySelector(idElementoAudio)
+}
+
+document.querySelector('.tecla_pom');{
    
    document.querySelector('#som_tecla_pom').play('');
-   
-   
+}
+   document.querySelectorAll('.tecla')  
+   const listadetecla = document.querySelectorAll('.tecla')
+
+   listadetecla[0].onclick = tocaSomPom;
+
+   while(contador < 9){
+      listadetecla[contador].onclick = function(){
+
+         tocaSomPom('#som_tecla_clap');
+      }
+
+      contador = contador + 1;
+   }
 
